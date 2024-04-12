76517821dd70e6f7ae8916574e40c16459fd6ef7
Thiago <thiago.marim2005@gmail.com>
2024-04-12 14:53:48 -0300

    feat: added useForm

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,25 +1,30 @@
-'use server';
+"use server";
 
-import { cookies } from 'next/headers';
+import { TOKEN_POST } from "@/functions/api";
+import apiError from "@/functions/api-error";
+import { cookies } from "next/headers";
 
-export default async function login(formData: FormData) {
-  const username = formData.get('username') as string | null;
-  const password = formData.get('password') as string | null;
+export default async function login(state: {}, formData: FormData) {
+  const username = formData.get("username") as string | null;
+  const password = formData.get("password") as string | null;
 
-  const response = await fetch(
-    'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
-    {
-      method: 'POST',
+  try {
+    if (!username || !password) throw new Error("Preencha os dados.");
+    const { url } = TOKEN_POST();
+    const response = await fetch(url, {
+      method: "POST",
       body: formData,
-    },
-  );
-  const data = await response.json();
-  cookies().set('token', data.token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'lax',
-    maxAge: 60 * 60 * 24,
-  });
-
-  console.log(data);
+    });
+    if (!response.ok) throw new Error("Senha e usuário inválidos");
+    const data = await response.json();
+    cookies().set("token", data.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24,
+    });
+    return { data: null, ok: true, error: "" };
+  } catch (error: unknown) {
+    return apiError(error);
+  }
 }
