8e61b114bd282dc2eeeea1d13833b6830b55398b
Thiago <thiago.marim2005@gmail.com>
2024-04-12 15:50:09 -0300

    feat: added reset password

--- src/app/login/resetar/page.tsx
+++ src/app/login/resetar/page.tsx
@@ -1,7 +1,28 @@
-export default async function ResetarPage() {
+import LoginResetarForm from "@/components/login/login-resetar-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Resetar a senha | Dogs",
+  description: "Resete a sua senha",
+};
+
+type ResetarSearchParams = {
+  searchParams: {
+    key: string;
+    login: string;
+  };
+};
+
+export default async function ResetarPage({
+  searchParams,
+}: ResetarSearchParams) {
   return (
-    <main>
-      <h1>Resetar</h1>
-    </main>
+    <div className="animeLeft">
+      <h1 className="title">Resete a Senha</h1>
+      <LoginResetarForm
+        keyToken={searchParams.key}
+        login={searchParams.login}
+      />
+    </div>
   );
 }
