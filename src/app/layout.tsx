1140cc5fe287143d584f7b5afcca3f0da18dca8a
Thiago <thiago.marim2005@gmail.com>
2024-04-12 18:43:49 -0300

    feat: added global context for user data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,31 @@ import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
+import { UserContextProvider } from "@/context/user-context";
+import userGet from "@/actions/user-get";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
   description: "Rede social para cachorros",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: user } = await userGet();
+
   return (
     <html lang="pt-BR">
       <body className={type_second.variable}>
-        <div className="App">
-          <Header />
-          <main className="AppBody">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );
