73a208c0cabefd2ca26602dda28ac3560fd5d23c
Thiago <thiago.marim2005@gmail.com>
2024-04-13 18:12:17 -0300

    feat: added photo content

--- src/components/feed/feed.tsx
+++ src/components/feed/feed.tsx
@@ -1,9 +1,10 @@
-"use client";
+'use client';
 
-import photosGet, { Photo } from "@/actions/photos-get";
-import FeedPhotos from "./feed-photos";
-import React from "react";
-import Loading from "../helper/loading";
+import photosGet, { Photo } from '@/actions/photos-get';
+import FeedPhotos from './feed-photos';
+import React from 'react';
+import Loading from '@/components/helper/loading';
+import styles from './feed.module.css';
 
 export default function Feed({
   photos,
@@ -16,12 +17,12 @@ export default function Feed({
   const [page, setPage] = React.useState(1);
   const [loading, setLoading] = React.useState(false);
   const [infinite, setInfinite] = React.useState(
-    photos.length < 6 ? false : true
+    photos.length < 6 ? false : true,
   );
 
   const fetching = React.useRef(false);
   function infiniteScroll() {
-    console.log("aconteceu");
+    console.log('aconteceu');
     if (fetching.current) return;
     fetching.current = true;
     setLoading(true);
@@ -38,8 +39,8 @@ export default function Feed({
       const actionData = await photosGet(
         { page, total: 6, user: 0 },
         {
-          cache: "no-store",
-        }
+          cache: 'no-store',
+        },
       );
       if (actionData && actionData.data !== null) {
         const { data } = actionData;
@@ -52,22 +53,24 @@ export default function Feed({
 
   React.useEffect(() => {
     if (infinite) {
-      window.addEventListener("scroll", infiniteScroll);
-      window.addEventListener("wheel", infiniteScroll);
+      window.addEventListener('scroll', infiniteScroll);
+      window.addEventListener('wheel', infiniteScroll);
     } else {
-      window.removeEventListener("scroll", infiniteScroll);
-      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener('scroll', infiniteScroll);
+      window.removeEventListener('wheel', infiniteScroll);
     }
     return () => {
-      window.removeEventListener("scroll", infiniteScroll);
-      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener('scroll', infiniteScroll);
+      window.removeEventListener('wheel', infiniteScroll);
     };
   }, [infinite]);
 
   return (
     <div>
       <FeedPhotos photos={photosFeed} />
-      {loading && <Loading />}
+      <div className={styles.loadingWrapper}>
+        {infinite ? loading && <Loading /> : <p>Não existem mais postagens.</p>}
+      </div>
     </div>
   );
 }
